5b7a491ba74eb04311e094f047ce1799525e59f6
Musa Akbari <>
2020-07-14 14:39:19 -0700

    remove youtube related videos

--- src/static/Videos.ts
+++ src/static/Videos.ts
@@ -8,19 +8,19 @@ type VideoData = {
 export const FieldVideos: VideoData[] = [
   {
     title: "Scan and Piece Out Process",
-    source: `https://www.youtube.com/embed/${"OLOat8g9h6Q"}`,
+    source: `https://www.youtube.com/embed/${"OLOat8g9h6Q"}?rel=0`,
     mdDelay: 0,
     smDelay: 100
   },
   {
     title: "Complete Process in the Field",
-    source: `https://www.youtube.com/embed/${"WENGcUzHkR8"}`,
+    source: `https://www.youtube.com/embed/${"WENGcUzHkR8"}?rel=0`,
     mdDelay: 200,
     smDelay: 100
   },
   {
     title: "Face Scan Clock In Demo",
-    source: `https://www.youtube.com/embed/${"NSDl5QBbg24"}`,
+    source: `https://www.youtube.com/embed/${"NSDl5QBbg24"}?rel=0`,
     mdDelay: 400,
     smDelay: 100
   },
@@ -28,7 +28,7 @@ export const FieldVideos: VideoData[] = [
 
 export const LandingVideo: VideoData = {
   title: "PET Tiger Intro",
-  source: `https://www.youtube.com/embed/${"kOOKIl9RbD4"}`,
+  source: `https://www.youtube.com/embed/${"kOOKIl9RbD4"}?rel=0`,
   mdDelay: 0,
   smDelay: 0
 }
